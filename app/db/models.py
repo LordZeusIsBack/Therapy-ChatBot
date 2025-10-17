20ff3187e4ec926ddd30326362a6288f31824b25
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-10-17 09:09:34 +0530

    Added a User Table

--- app/db/models.py
+++ app/db/models.py
@@ -12,3 +12,15 @@ class HMACTable(Base):
     salt = Column(String, nullable=False)
     hmac_key = Column(String, nullable=False)
     user = relationship('User', back_populates='hmac', uselist=False)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    hmac_id = Column(Integer, ForeignKey('hmac_keys.id'))
+    encrypted_name = Column(String, nullable=False)
+    encrypted_email = Column(String, nullable=False)
+    encrypted_dob = Column(String)
+
+    hmac = relationship('HMACTable', back_populates='user')
